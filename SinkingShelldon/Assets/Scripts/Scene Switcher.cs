50a482e11d36e6515391fdfc6ef89542adee88c2
JohnV <1010715@student.sae.edu.au>
2018-08-14 13:50:33 +0800

    Initial Upload

---
+++ SinkingShelldon/Assets/Scripts/Scene Switcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+
+
+	void Start ()
+    {
+	
+        
+
+	}
+
+	void Update ()
+    {
+		
+
+
+	}
+
+    public void ToGameScene()
+    {
+
+        SceneManager.LoadScene("GameScene");
+
+    }
+}
